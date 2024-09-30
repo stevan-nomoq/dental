e35860d1a4f956c0cabdc9702429b2b1c736bb69
antelo <david@sotelo.io>
2024-09-30 16:40:44 +0200

    Add new sale order line if it is open

--- dental_practice/models/dental.py
+++ dental_practice/models/dental.py
@@ -11,15 +11,24 @@ class Patient(models.Model):
     def write(self, vals):
         res = super(Patient, self).write(vals)
         if 'treat_id' in vals:
-            # if there is an open bill add a new line, if not open a new so
-            sale_order = self.env['sale.order'].create({
-                'partner_id': self.id,
-                'order_line': [(0, 0, {
-                    'product_id': vals['treat_id'],
-                })],
-            })
+            # if there is an open bill add a new line, if not, open a new SO
+            existing_so = self.env['sale.order'].search([('partner_id', '=', self.id),('state', 'in', ['draft', 'sent'])])
+            if existing_so:
+                # if we need to add one or multiple sale order lines in the DB
+                self.env["sale.order.line"].create(
+                    {
+                        "order_id": existing_so.id,
+                        "product_id": vals["treat_id"],  # some product.product ID,
+                    }
+                )
+            else:
+                sale_order = self.env['sale.order'].create({
+                    'partner_id': self.id,
+                    'order_line': [(0, 0, {
+                        'product_id': vals['treat_id'],
+                    })],
+                })
         return res
-
 class Diags(models.Model):
     _name = "dental.diags"
 
